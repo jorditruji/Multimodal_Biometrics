f78cb8e211407f7853f40b09c249de8810b93acb
jorditruji <you@example.com>
2018-10-12 01:36:35 +0200

    mfcc

--- segan_biometrics.py
+++ segan_biometrics.py
@@ -50,6 +50,7 @@ class MiniConvNet(nn.Module):
 		out = self.layer2(out)
 		out = self.layer3(out)
 		out = self.layer4(out)
+		print out.shape
 		out = out.reshape(out.size(0), -1)
 		print out.shape
 		out = self.fc(out)
