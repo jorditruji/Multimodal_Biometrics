9a1d9824869bef639293ba7e96758d3130c42210
jorditruji <you@example.com>
2018-10-12 00:54:15 +0200

    mfcc

--- segan_biometrics.py
+++ segan_biometrics.py
@@ -77,6 +77,7 @@ def train_model(model, criterion, optimizer,scheduler, num_epochs=25):
 	best_acc = 0.0
 
 	model = model.to(device)
+	model.cuda()
 	max_epochs=50
 	#Loop over epochs
 	for epoch in range(max_epochs):
