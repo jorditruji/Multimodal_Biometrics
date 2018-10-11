30804f122e856cdfd4f5c51e3c454f2ed2edc838
jorditruji <you@example.com>
2018-10-11 19:30:12 +0200

    audio branch

---
+++ Audio/segan_biometrics.py
@@ -0,0 +1,21 @@
+import torch
+from torch.utils import data
+from Data_Management.data_utils import load_partitions
+from my_classes import Dataset
+
+
+# CUDA for PyTorch
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda:0" if use_cuda else "cpu")
+cudnn.benchmark = True
+
+# Parameters
+params = {'batch_size': 64,
+          'shuffle': True,
+          'num_workers': 6}
+max_epochs = 100
+
+
+data,labels=load_partitions()
+print (labels.keys())
+
