52add83fa95a4764e0873197df95e28842c051a1
jorditruji <you@example.com>
2018-10-11 20:27:58 +0200

    fuck imports on imython

--- segan_biometrics.py
+++ segan_biometrics.py
@@ -6,7 +6,6 @@ from torch.utils import data
 # CUDA for PyTorch
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda:0" if use_cuda else "cpu")
-cudnn.benchmark = False
 
 # Parameters
 params = {'batch_size': 64,
@@ -18,3 +17,19 @@ max_epochs = 100
 data,labels=load_partitions()
 print (labels.keys())
 
+
+# Generators
+training_set = Dataset(partition['train'], labels)
+training_generator = data.DataLoader(training_set, **params)
+
+validation_set = Dataset(partition['validation'], labels)
+validation_generator = data.DataLoader(validation_set, **params)
+
+# Loop over epochs
+for epoch in range(max_epochs):
+    # Training
+    for local_batch, local_labels in training_generator:
+        # Transfer to GPU
+        local_batch, local_labels = local_batch.to(device), local_labels.to(device)
+
+        # Model computations
