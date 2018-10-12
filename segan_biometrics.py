832199bd42772d32122a616ea319c5bb34837d35
jorditruji <you@example.com>
2018-10-12 15:37:51 +0200

    fixed validation

--- segan_biometrics.py
+++ segan_biometrics.py
@@ -67,8 +67,6 @@ def train_model(model, criterion, optimizer,scheduler, num_epochs=25):
 			with torch.set_grad_enabled(True):
 				outputs = model(local_batch)
 				_, preds = torch.max(outputs, 1)
-				local_labels=torch.tensor(to_categorical(local_labels, 27), device=device).type(torch.cuda.LongTensor)
-				print local_labels.shape, outputs.shape
 				loss = criterion(outputs, local_labels)
 				# backward + optimize only if in training phase
 				loss.backward()
@@ -129,6 +127,7 @@ def train_model(model, criterion, optimizer,scheduler, num_epochs=25):
 	model.load_state_dict(best_model_wts)
 	return model.cuda()
 
+
 # CUDA for PyTorch
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda:0" if use_cuda else "cpu")
@@ -142,7 +141,7 @@ params = {'batch_size': 64,
 
 partition,labels=load_partitions()
 
-
+print max(labels[labels.keys()]),min(labels[labels.keys()])
 
 # Generators
 training_set = Dataset(partition['train'], labels)
@@ -154,7 +153,6 @@ validation_generator = data.DataLoader(validation_set, **params)
 
 
 model = MiniConvNet(num_classes=27).to(device)
-
 
 model_ft = model.to(device)
 
