24a0df2ffe395bed513ba23123dcf63fbf21bb9e
jorditruji <you@example.com>
2018-10-12 18:21:49 +0200

    mfcc

--- Data_Management/dataset.py
+++ Data_Management/dataset.py
@@ -71,7 +71,7 @@ class Dataset(data.Dataset):
         if self.mfcc:
             
             mfcc_matric=mfcc(wav_data,samplerate=fm,numcep=64)
-            mfcc_matric=mfcc_matric / np.max(mfcc_matric)
+            mfcc_matric=(mfcc_matric - np.mean(mfcc_matric)) / np.std(mfcc_matric)
             
             return mfcc_matric,y
 
--- segan_biometrics.py
+++ segan_biometrics.py
@@ -173,7 +173,7 @@ criterion = nn.CrossEntropyLoss()
 print list(model.parameters())
 
 # Observe that all parameters are being optimized
-optimizer_ft = optim.Adam(model_ft.parameters())
+optimizer_ft = optim.Adam(model_ft.parameters(), lr=0.0001)
 
 # Decay LR by a factor of 0.1 every 7 epochs
 exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
