b4f38f65ee3cc3fadaf5b436da30f8b13fb876ca
jorditruji <you@example.com>
2018-10-12 19:36:52 +0200

    mfcc

--- segan_biometrics.py
+++ segan_biometrics.py
@@ -172,7 +172,7 @@ criterion = nn.CrossEntropyLoss()
 print list(model.parameters())
 
 # Observe that all parameters are being optimized
-optimizer_ft = optim.Adagrad(model_ft.parameters(), lr=0.001, lr_decay=0)
+optimizer_ft = optim.Adagrad(model_ft.parameters(), lr=0.0001, lr_decay=0)
 
 # Decay LR by a factor of 0.1 every 7 epochs
 exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
