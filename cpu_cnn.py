db651c1700c135a26019840a87888011ad013b3b
jorditruji <you@example.com>
2018-10-13 14:19:09 +0200

    fran net

--- cpu_cnn.py
+++ cpu_cnn.py
@@ -61,7 +61,7 @@ def train_model(model, criterion, optimizer,scheduler, num_epochs=25):
 			# forward + shapes modification...
 			local_batch=local_batch.unsqueeze_(1)
 			local_batch=local_batch.type(torch.FloatTensor)
-			outputs = model(local_batch)
+			outputs,int_act = model(local_batch)
 			print "segan outs:"
 			print outputs
 			print "segan embed shape"
