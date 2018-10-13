dd2ba2ef9ffbebb8e6e8d6e989de6465062084de
jorditruji <you@example.com>
2018-10-13 11:17:15 +0200

    testing

--- Data_Management/dataset.py
+++ Data_Management/dataset.py
@@ -72,6 +72,7 @@ class Dataset(data.Dataset):
             
             mfcc_matric=mfcc(wav_data,samplerate=fm,numcep=12)
             mfcc_matric=(mfcc_matric - np.mean(mfcc_matric)) / np.std(mfcc_matric)
+            print mfcc_matric.shape
             return mfcc_matric,y
 
 
