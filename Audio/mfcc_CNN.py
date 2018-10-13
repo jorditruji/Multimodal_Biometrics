031a28adc9b5a601856dc4016aecee768c75dec4
jorditruji <you@example.com>
2018-10-13 12:33:07 +0200

    keras+ cpu versions

--- Audio/mfcc_CNN.py
+++ Audio/mfcc_CNN.py
@@ -135,7 +135,7 @@ class MiniConvNet2(nn.Module):
 			nn.MaxPool2d(kernel_size=(4,2)))
 
 		self.fc = nn.Sequential(
-			nn.Linear(448,1024),
+			nn.Linear(96,1024),
 			nn.ReLU(),
 			nn.Dropout(),
 			nn.Linear(1024,num_classes),
