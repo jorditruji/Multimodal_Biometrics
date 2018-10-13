5126efb8fa53706efcb0d0bb61eb51794d847d7d
jorditruji <you@example.com>
2018-10-13 12:29:57 +0200

    keras+ cpu versions

--- Audio/mfcc_CNN.py
+++ Audio/mfcc_CNN.py
@@ -129,10 +129,10 @@ class MiniConvNet2(nn.Module):
 			nn.ReLU(),
 			nn.MaxPool2d(kernel_size=(2,4)))
 		self.layer4 = nn.Sequential(
-			nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=2),
-			nn.BatchNorm2d(64),
+			nn.Conv2d(64, 32, kernel_size=3, stride=1, padding=2),
+			nn.BatchNorm2d(32),
 			nn.ReLU(),
-			nn.MaxPool2d(kernel_size=(1,2)))
+			nn.MaxPool2d(kernel_size=(1,4)))
 
 		self.fc = nn.Sequential(
 			nn.Linear(120,512),
