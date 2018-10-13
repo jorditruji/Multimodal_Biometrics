f369f9a08aaf4e1397eba841307ad717a0a77897
jorditruji <you@example.com>
2018-10-13 11:09:50 +0200

    testing

--- segan_biometrics.py
+++ segan_biometrics.py
@@ -72,6 +72,7 @@ def train_model(model, criterion, optimizer,scheduler, num_epochs=25):
 
 			for param in model.parameters():
 				print(param.grad.data.sum())
+			
 			optimizer.step()
 			scheduler.step()
 			b = list(model.parameters())[0].clone()
@@ -170,10 +171,9 @@ model_ft = model.to(device)
 
 criterion = nn.CrossEntropyLoss()
 
-print list(model.parameters())
 
 # Observe that all parameters are being optimized
-optimizer_ft = optim.Adagrad(model_ft.parameters(), lr=0.0001, lr_decay=0)
+optimizer_ft = optim.Adam(model_ft.parameters(), lr=0.0001, lr_decay=0)
 
 # Decay LR by a factor of 0.1 every 7 epochs
 exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
