eff6d2f508e21b442d63e09bd3d9f681525836ed
jorditruji <you@example.com>
2018-10-15 21:20:58 +0200

    spectrograms

--- Data_Management/dataset.py
+++ Data_Management/dataset.py
@@ -70,7 +70,7 @@ class Dataset(data.Dataset):
             spectogram=feat_ex.extract(wav_data)
             #mfcc_matric=mfcc(wav_data,samplerate=fm,numcep=32)
             #mfcc_matric=(mfcc_matric - np.mean(mfcc_matric)) / np.std(mfcc_matric)
-            return mfcc_matric,y
+            return spectogram,y
         else:
             # Some preprocessing
             #if self.preprocessing:
