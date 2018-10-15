16fcaafebdb1c2a233a95e83e7d2abda398e0ef2
jorditruji <you@example.com>
2018-10-15 22:18:04 +0200

    spectrograms

--- Audio/mfcc_CNN.py
+++ Audio/mfcc_CNN.py
@@ -2,18 +2,65 @@ import torch
 import torch.nn as nn
 
 
+class MiniVGG(nn.Module):
+	def __init__(self, num_classes=27):
+		super(MiniVGG,self).__init__()
+		self.conv1 = nn.Sequential(
+			nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
+			nn.BatchNorm2d(32),
+			nn.ReLU(),
+			nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
+			nn.BatchNorm2d(32),
+			nn.ReLU(),
+			nn.MaxPool2d(kernel_size=2, stride=2)
+		)
+		self.conv2 = nn.Sequential(
+			nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+			nn.BatchNorm2d(64),
+			nn.ReLU(),
+			nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+			nn.BatchNorm2d(64),
+			nn.ReLU(),
+			nn.MaxPool2d(kernel_size=2, stride=2)
+		)
+		self.conv3 = nn.Sequential(
+			nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+			nn.BatchNorm2d(128),
+			nn.ReLU(),
+			nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+			nn.BatchNorm2d(128),
+			nn.ReLU(),
+			nn.MaxPool2d(kernel_size=2, stride=2)
+		)
+		self.fc = nn.Sequential(
+			#nn.AvgPool2d(kernel_size=7, stride=1, padding=0),
+			nn.Linear(128,1024),
+			nn.ReLU(),
+			nn.Dropout(),
+			nn.Linear(1024,num_classes),
+			torch.nn.Softmax())
+
+	def forward(self, x):
+        out = self.conv1(x)
+        out = self.conv2(out)
+        out = self.conv3(out)
+        out = out.reshape(out.size(0), -1)
+        out = self.fc(out)
+        return out
+
+
 # Convolutional neural network (two convolutional layers)
 class ConvNet(nn.Module):
     def __init__(self, num_classes=27):
         super(ConvNet, self).__init__()
         self.layer1 = nn.Sequential(
-            nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=2),
-            nn.BatchNorm2d(16),
+            nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
 
         self.layer2 = nn.Sequential(
-            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=2),
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=2),
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=3, stride=3))
@@ -46,7 +93,6 @@ class ConvNet(nn.Module):
             torch.nn.Softmax())
         
     def forward(self, x):
-    	print x
         out = self.layer1(x)
         out = self.layer2(out)
         out = self.layer3(out)
--- cpu_cnn.py
+++ cpu_cnn.py
@@ -169,7 +169,7 @@ validation_generator = data.DataLoader(validation_set, **params)
 
 d_fmaps = [16, 32, 128, 256, 512, 1024]
 
-model_ft = models.resnet18(pretrained=True)
+model_ft = models.MiniVGG()
 num_ftrs = model_ft.fc.in_features
 model_ft.fc = nn.Linear(num_ftrs, 27)
 model_ft = model_ft.to(device)
