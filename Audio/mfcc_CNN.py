0eeac6ffa6fbb1d1f5966daf2a25fb1590a54f64
jorditruji <you@example.com>
2018-10-16 18:45:35 +0200

    spectrograms

--- Audio/mfcc_CNN.py
+++ Audio/mfcc_CNN.py
@@ -200,33 +200,33 @@ class MiniConvNet2(nn.Module):
 
 
 class BasicBlock(nn.Module):
-    expansion = 1
+	expansion = 1
 
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
-        super(BasicBlock, self).__init__()
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.relu = ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.downsample = downsample
-        self.stride = stride
+	def __init__(self, inplanes, planes, stride=1, downsample=None):
+		super(BasicBlock, self).__init__()
+		self.conv1 = conv3x3(inplanes, planes, stride)
+		self.bn1 = nn.BatchNorm2d(planes)
+		self.relu = ReLU(inplace=True)
+		self.conv2 = conv3x3(planes, planes)
+		self.bn2 = nn.BatchNorm2d(planes)
+		self.downsample = downsample
+		self.stride = stride
 
-    def forward(self, x):
-        residual = x
+	def forward(self, x):
+		residual = x
 
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
+		out = self.conv1(x)
+		out = self.bn1(out)
+		out = self.relu(out)
 
-        out = self.conv2(out)
-        out = self.bn2(out)
+		out = self.conv2(out)
+		out = self.bn2(out)
 
-        if self.downsample is not None:
-            residual = self.downsample(x)
+		if self.downsample is not None:
+		    residual = self.downsample(x)
 
-        out += residual
-        out = self.relu(out)
+		out += residual
+		out = self.relu(out)
 
 		return out
 
