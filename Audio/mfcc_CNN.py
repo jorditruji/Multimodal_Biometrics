f3999729e67432a72bb0754d7bd2efab9a1729cc
jorditruji <you@example.com>
2018-10-16 18:48:26 +0200

    spectrograms

--- Audio/mfcc_CNN.py
+++ Audio/mfcc_CNN.py
@@ -199,6 +199,12 @@ class MiniConvNet2(nn.Module):
 		return out
 
 
+def conv3x3(in_planes, out_planes, stride=1):
+    "3x3 convolution with padding"
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False)
+
+
 class BasicBlock(nn.Module):
 	expansion = 1
 
