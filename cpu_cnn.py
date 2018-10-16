70fc5e27e2a7421d8ad6ebad0d05dbf4ed59f317
jorditruji <you@example.com>
2018-10-16 18:43:01 +0200

    spectrograms

--- cpu_cnn.py
+++ cpu_cnn.py
@@ -168,7 +168,7 @@ validation_generator = data.DataLoader(validation_set, **params)
 
 d_fmaps = [16, 32, 128, 256, 512, 1024]
 
-model_ft = MiniVGG()
+model_ft = DeepSpeakerModel()#MiniVGG()
 
 model_ft = model_ft.to(device)
 
