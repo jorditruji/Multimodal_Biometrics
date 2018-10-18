a65415a3c24658ee0abe2b9bec6b46b9df5219d9
jorditruji <you@example.com>
2018-10-18 18:30:24 +0200

    tabs

--- cpu_cnn.py
+++ cpu_cnn.py
@@ -73,7 +73,7 @@ def train_model(model, criterion, optimizer,scheduler, num_epochs=25):
 			a = list(model.parameters())[12].clone()
 
 			loss.backward()
-			for p,n in enumerate(model.parameters()):
+			for n,p in enumerate(model.parameters()):
 				print n,p.grad
 
 			optimizer.step()
